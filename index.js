484383c74e3f5b1511d8b5916245d19774379e7a
Bhupesh Choudhary <bhaibhupesh10@gmail.com>
2024-12-11 16:16:46 +0530

    refactor:[new routes updated]

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const express = require("express");
 const app = express();   // function 
  // server will listen on this port 
 
+
+ const JSONDATA = {
+    "name": "Bhupesh Choudhary", 
+    "mob": "7489356891",
+    "addr": "jaipur"
+ }
+
+
+
+
 app.get("/", (req, res)=>{          // listen on slash route if you get any request on then it will send the response
     res.send("Hello World");
 });
@@ -18,6 +28,11 @@ app.get("/login", (req, res)=>{
 app.get("/youtube", (req, res)=>{
     res.send("<h2>Chai aur code</h1>");
 })
+
+app.get("/mydata", (req, res)=>{
+    res.json(JSONDATA);
+})
+
 app.listen(process.env.PORT, ()=>{              // listen (variable)
     console.log(`listening to port ${process.env.PORT}`);
 });
